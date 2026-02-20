10cf9d63177704aba128038ea88be9b3f674cecf
Austin <greenea21@uww.edu>
2026-02-20 13:30:35 -0600

    MVP Task 3

--- main.cpp
+++ main.cpp
@@ -20,6 +20,8 @@ Mass:
 //-------------------//
 
 int main() {
+	string temp;
+
 	//MVP Task 1
 	cout << "Please select the type of measurement you wish to convert" << endl;
 	cout << "(Type the associated letter and hit enter)" << endl;
@@ -39,6 +41,14 @@ int main() {
 	string user_unit_type_selection;
 	getline(cin, user_unit_type_selection);
 	cout << "You selected: " << user_unit_type_selection << endl;
+
+	//MVP Task 3
+	cout << setprecision(16); //Using this without "fixed" shows only relevant user-entered decimal places; Even NASA uses no more than 16 decimal places so this should be plenty accurate
+	cout << endl;
+	cout << "Enter the NUMERICAL value to be converted: ";
+	getline(cin, temp);
+	double user_value = stod(temp);
+	cout << "You entered: " << user_value << endl;
 
 	return 0;
 }
