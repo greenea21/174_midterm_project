b44b968a92b6e451921e26ab6588a462589bdc53
Austin <greenea21@uww.edu>
2026-02-20 12:45:57 -0600

    Conversion table

--- main.cpp
+++ main.cpp
@@ -2,6 +2,21 @@
 #include <string>
 #include <iomanip>
 
+//-------------------//
+/*
+
+Distance:
+1ft = 0.3048m
+
+Temp:
+F = (9/5 * C) + 32
+
+Mass:
+1lb = 453.592g
+
+*/
+//-------------------//
+
 int main() {
 
 
